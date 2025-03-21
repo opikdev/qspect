6af081a7b7745d44e63f258989d2a9b5d5dfbb13
Opik <hi@opik.dev>
2025-03-21 12:18:42 +0700

    feat: inject content script on action click

--- src/background/main.ts
+++ src/background/main.ts
@@ -6,21 +6,93 @@ if (import.meta.hot) {
   import('./contentScriptHMR')
 }
 
-// remove or turn this off if you don't use side panel
-const USE_SIDE_PANEL = false
+const injectedTabs = new Set()
 
-// to toggle the sidepanel with the action button in chromium:
-if (USE_SIDE_PANEL) {
-  // @ts-expect-error missing types
-  browser.sidePanel
-    .setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error: unknown) => console.error(error))
-}
+browser.action.onClicked.addListener(async (tab) => {
+  if (!tab.id)
+    return
 
-browser.runtime.onInstalled.addListener((): void => {
-  // eslint-disable-next-line no-console
-  console.log('Extension installed')
+  try {
+    const [result] = await browser.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => !!document.querySelector('#qspect'),
+    })
+
+    const isContentScriptPresent = result.result
+
+    if (injectedTabs.has(tab.id) && isContentScriptPresent) {
+      await browser.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          const root = document.querySelector('#qspect')
+          if (root) {
+            root.remove()
+          }
+        },
+      })
+
+      await browser.action.setBadgeText({
+        tabId: tab.id,
+        text: '',
+      })
+
+      injectedTabs.delete(tab.id)
+
+      // eslint-disable-next-line no-console
+      console.log('Content script removed successfully')
+    }
+    else {
+      try {
+        await browser.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ['dist/contentScripts/index.global.js'],
+        })
+
+        await browser.action.setBadgeText({
+          tabId: tab.id,
+          text: 'ON',
+        })
+
+        await browser.action.setBadgeBackgroundColor({
+          tabId: tab.id,
+          color: 'green',
+        })
+
+        injectedTabs.add(tab.id)
+
+        // eslint-disable-next-line no-console
+        console.log('Content script injected successfully')
+      }
+      catch (injectionError) {
+        console.error('Failed to inject content script:', injectionError)
+        throw injectionError
+      }
+    }
+  }
+  catch (error) {
+    console.error('Failed to toggle content script:', error)
+  }
 })
+
+browser.tabs.onRemoved.addListener((tabId) => {
+  injectedTabs.delete(tabId)
+})
+
+// // remove or turn this off if you don't use side panel
+// const USE_SIDE_PANEL = false
+
+// // to toggle the sidepanel with the action button in chromium:
+// if (USE_SIDE_PANEL) {
+//   // @ts-expect-error missing types
+//   browser.sidePanel
+//     .setPanelBehavior({ openPanelOnActionClick: true })
+//     .catch((error: unknown) => console.error(error))
+// }
+
+// browser.runtime.onInstalled.addListener((): void => {
+//   // eslint-disable-next-line no-console
+//   console.log('Extension installed')
+// })
 
 // let previousTabId = 0
 
