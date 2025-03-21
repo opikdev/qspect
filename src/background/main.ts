ab33017ad9589ff368dcb6f8480baf9a283a9f84
Opik <hi@opik.dev>
2025-03-21 11:59:07 +0700

    feat: disable prev tab event

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,6 +1,3 @@
-import type { Tabs } from 'webextension-polyfill'
-import { onMessage, sendMessage } from 'webext-bridge/background'
-
 // only on dev mode
 if (import.meta.hot) {
   // @ts-expect-error for background HMR
@@ -25,41 +22,41 @@ browser.runtime.onInstalled.addListener((): void => {
   console.log('Extension installed')
 })
 
-let previousTabId = 0
+// let previousTabId = 0
 
 // communication example: send previous tab title from background page
 // see shim.d.ts for type declaration
-browser.tabs.onActivated.addListener(async ({ tabId }) => {
-  if (!previousTabId) {
-    previousTabId = tabId
-    return
-  }
+// browser.tabs.onActivated.addListener(async ({ tabId }) => {
+//   if (!previousTabId) {
+//     previousTabId = tabId
+//     return
+//   }
 
-  let tab: Tabs.Tab
+//   let tab: Tabs.Tab
 
-  try {
-    tab = await browser.tabs.get(previousTabId)
-    previousTabId = tabId
-  }
-  catch {
-    return
-  }
+//   try {
+//     tab = await browser.tabs.get(previousTabId)
+//     previousTabId = tabId
+//   }
+//   catch {
+//     return
+//   }
 
-  // eslint-disable-next-line no-console
-  console.log('previous tab', tab)
-  sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId })
-})
+//   // eslint-disable-next-line no-console
+//   console.log('previous tab', tab)
+//   sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId })
+// })
 
-onMessage('get-current-tab', async () => {
-  try {
-    const tab = await browser.tabs.get(previousTabId)
-    return {
-      title: tab?.title,
-    }
-  }
-  catch {
-    return {
-      title: undefined,
-    }
-  }
-})
+// onMessage('get-current-tab', async () => {
+//   try {
+//     const tab = await browser.tabs.get(previousTabId)
+//     return {
+//       title: tab?.title,
+//     }
+//   }
+//   catch {
+//     return {
+//       title: undefined,
+//     }
+//   }
+// })
